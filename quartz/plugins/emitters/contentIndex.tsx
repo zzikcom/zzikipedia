239bfc37c05a85630adbee0db7c6700f8db1ad67
zzik.com <zzik.com@messymail.com>
2026-01-15 17:00:06 +0900

    modify explorer component (4)

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -143,8 +143,9 @@ export const ContentIndex: QuartzEmitterPlugin<Partial<Options>> = (opts) => {
           // remove description and from content index as nothing downstream
           // actually uses it. we only keep it in the index as we need it
           // for the RSS feed
-          delete content.description
-          delete content.date
+          
+          // delete content.description
+          // delete content.date
           return [slug, content]
         }),
       )
